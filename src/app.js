c7d20c644faf27ed2aee28c5606dff5efd20289b
Harshit <h008@Harshits-MacBook-Air.local>
2024-12-02 11:37:52 +0530

    undefined check

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ require('dotenv').config();
 const app = express();
 app.use(bodyParser.json());
 app.use(cors({
-  origin: process.env.FRONTEND_URL,
+  origin: process.env.FRONTEND_URL || "http://localhost:5173",
   credentials: true,
 }));
 
