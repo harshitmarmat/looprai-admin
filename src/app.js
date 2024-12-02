8091935b681ca2e878430848f6cbb7cc643c1bbc
Harshit <h008@Harshits-MacBook-Air.local>
2024-12-02 12:16:27 +0530

    added port

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ require('dotenv').config();
 const app = express();
 app.use(bodyParser.json());
 app.use(cors({
-  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  origin: process.env.FRONTEND_URL || "http://localhost:5173" || "http://localhost:4173",
   credentials: true,
 }));
 
